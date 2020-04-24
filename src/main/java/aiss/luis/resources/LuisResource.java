a3fbdca034e7f5d70ee6c0b7218a6d5bed0bfac5
Antonio González Gómez <antoniogg696@gmail.com>
2020-04-24 12:24:57 +0200

    Finished retrieveIntent method from LUISResource
    All Intent implementations but CreateMontageIntent can now be created from a JSON file

--- src/main/java/aiss/luis/resources/LuisResource.java
+++ src/main/java/aiss/luis/resources/LuisResource.java
@@ -20,31 +20,59 @@ import aiss.luis.model.classes.MusicIntent;
 import aiss.luis.model.classes.SpecificThemeIntent;
 import aiss.luis.model.enumerates.IntentType;
 import aiss.luis.model.enumerates.MontageTheme;
+import aiss.luis.model.enumerates.MusicEnergy;
+import aiss.luis.model.enumerates.MusicMood;
+import aiss.luis.model.enumerates.MusicTempo;
+import aiss.luis.model.enumerates.Sentiment;
 
 public class LuisResource {
 
 	
 	public static Intent retrieveIntent(String json) throws IOException {
 		JsonNode query = new ObjectMapper().readTree(json);
+		JsonNode entities = query.get("prediction").get("entities");
 		IntentType tipo = IntentType.valueOf
 				(query.get("prediction").get("topIntent").textValue());
-		JsonNode nodo_themes = query.get("prediction").get("entities").get("MontageTheme");
+		JsonNode nodo_themes = entities.get("MontageTheme");
 		List<MontageTheme> ls = retrieveThemes(nodo_themes , new ArrayList<MontageTheme>());
+		Sentiment sent = Sentiment.valueOf(query.get("prediction").get("sentiment").get("label").textValue());
 		switch(tipo) {
 		case CreateMontage:
 			MontageCreateIntent mcr = new MontageCreateIntent();
 			return mcr;
 		case MontageThemeIntent:
-			MontageThemeIntent mth = new MontageThemeIntent();
+			MontageThemeIntent mth = new MontageThemeIntent(sent, ls);
 			return mth;
 		case MusicDescription:
-			MusicIntent mus = new MusicIntent();
+			MusicTempo tempo = MusicTempo.none;
+			if (entities.has("MusicTempo"))
+				tempo = MusicTempo.valueOf(entities.get("MusicTempo").elements().next().fieldNames().next());
+			
+			MusicEnergy energy = MusicEnergy.none;
+			if (entities.has("MusicEnergy"))
+				energy = MusicEnergy.valueOf(entities.get("MusicEnergy").elements().next().fieldNames().next());
+			
+			MusicMood mood = MusicMood.none;
+			if (entities.has("MusicMood"))
+				mood = MusicMood.valueOf(entities.get("MusicMood").elements().next().fieldNames().next());
+			
+			Boolean danceable = false;
+			if (entities.has("MusicDanceable"))
+				danceable = true;
+			
+			List<String> ls_author = new ArrayList<String>();
+			Iterator<JsonNode> it_author = entities.get("MusicAuthor").elements();
+			while(it_author.hasNext()) {
+				ls_author.add(it_author.next().textValue());
+			}
+			
+			MusicIntent mus = new MusicIntent(sent, ls_author, mood, tempo, energy, danceable);
 			return mus;
 		case SpecificTheme:
-			SpecificThemeIntent sth = new SpecificThemeIntent();
+			SpecificThemeIntent sth = new SpecificThemeIntent(sent, ls);
 			return sth;
 		default:
-			Intent in = new Intent(tipo);
+			Intent in = new Intent(tipo, sent);
 			return in;
 		}
 	}
@@ -61,6 +89,8 @@ public class LuisResource {
 	   }
 	
 	private static List<MontageTheme> retrieveThemes(JsonNode nodo, List<MontageTheme> ls) {
+		if (nodo == null)
+			return ls;
 		Iterator<JsonNode> itnode = nodo.elements();
 		while(itnode.hasNext()) {
 			Iterator<Map.Entry<String,JsonNode>> itentry = itnode.next().fields();
@@ -72,8 +102,6 @@ public class LuisResource {
 					ls = retrieveThemes(entrada.getValue(), ls);
 			}
 		}
-		
-		
 		return ls;
 	}
 	/*
@@ -82,11 +110,10 @@ public class LuisResource {
 		    try
 		    {
 		        content = new String ( Files.readAllBytes( Paths.get("C:/Users/anton/Desktop/file.txt") ) );
-		        JsonNode productNode = new ObjectMapper().readTree(content);
-		        JsonNode array = productNode.get("prediction").get("entities").get("MontageTheme");
-		        System.out.println(retrieveThemes(array, new ArrayList<MontageTheme>()));
-		        
-		       
+		        JsonNode query = new ObjectMapper().readTree(content);
+		        JsonNode entities = query.get("prediction").get("entities");
+		        Intent in = retrieveIntent(content);
+		        System.out.println("ekere");
 		        
 		    } 
 		    catch (IOException e) 

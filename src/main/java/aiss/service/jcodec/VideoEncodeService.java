f505fd53f52de34decd119a6b06ce2d0561a96ef
Nicolás de Ory <deorynicolas@gmail.com>
2020-04-24 17:00:01 +0200

    Added Humble Video dependency

---
+++ src/main/java/aiss/service/jcodec/VideoEncodeService.java
@@ -0,0 +1,19 @@
+package aiss.service.jcodec;
+
+import java.io.IOException;
+
+import org.jcodec.common.io.FileChannelWrapper;
+import org.jcodec.common.io.NIOUtils;
+
+public class VideoEncodeService 
+{
+
+	/*private static RgbToYuv420 transform = new RgbToYuv420(0,0);
+	public static void Encode(String path) throws IOException
+	{
+		FileChannelWrapper ch = NIOUtils.writableFileChannel(path);
+		
+		
+	}*/
+	
+}

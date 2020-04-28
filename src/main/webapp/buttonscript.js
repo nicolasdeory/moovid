71a1d0f608b0dfb243795c6d2b774c69384e0f18
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2020-04-28 11:59:36 +0200

    Buttons now change the content of the right square

---
+++ src/main/webapp/buttonscript.js
@@ -0,0 +1,11 @@
+function luisAPI() {
+    document.getElementById("result").innerHTML = "HAS HECHO CLICK EN EL BOTÓN DE LUIS";
+  }
+
+function spotifyAPI(){
+    document.getElementById("result").innerHTML = "HAS HECHO CLICK EN EL BOTÓN DE SPOTIFY";
+}
+
+function photosAPI(){
+    document.getElementById("result").innerHTML = "HAS HECHO CLICK EN EL BOTÓN DE GOOGLE PHOTOS";
+}

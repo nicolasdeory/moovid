fa301319a0852d22c5f5d182505b87242da034fe
Nicolás de Ory <deorynicolas@gmail.com>
2020-04-29 21:42:12 +0200

    Fixed image resizing

--- src/main/java/aiss/service/video/ImageUtil.java
+++ src/main/java/aiss/service/video/ImageUtil.java
@@ -14,36 +14,18 @@ public class ImageUtil {
 		
 		// Resize the image (Fit bounds mode)
 
-		if (img.getWidth() > desiredWidth || img.getHeight() > desiredHeight) 
-		{
-			int factorWidth = desiredWidth / img.getWidth();
-			int factorHeight = desiredHeight / img.getHeight(); 
-			int factor = factorWidth * img.getHeight() > desiredHeight ? factorHeight : factorWidth;
-			
-			int finalWidth = img.getWidth() * factor;
-			int finalHeight = img.getHeight() * factor;
-			
-			int imageX = (desiredWidth - finalWidth) / 2;
-			int imageY = (desiredHeight - finalHeight) / 2;
-			Graphics2D g = resized.createGraphics(); // this generates a black background
-			g.drawImage(img, imageX, imageY, finalWidth, finalHeight, null);
-			g.dispose();
-			
-		} else
-		{
-			int factorWidth = desiredWidth / img.getWidth();
-			int factorHeight = desiredHeight / img.getHeight(); 
-			int factor = factorWidth * img.getHeight() > desiredHeight ? factorHeight : factorWidth;
-			
-			int finalWidth = img.getWidth() * factor;
-			int finalHeight = img.getHeight() * factor;
-			
-			int imageX = (desiredWidth - finalWidth) / 2;
-			int imageY = (desiredHeight - finalHeight) / 2;
-			Graphics2D g = resized.createGraphics(); // this generates a black background
-			g.drawImage(img, imageX, imageY, finalWidth, finalHeight, null);
-			g.dispose();
-		}
+		float factorWidth = (float)desiredWidth / img.getWidth();
+		float factorHeight = (float)desiredHeight / img.getHeight(); 
+		float factor = factorWidth * img.getHeight() > desiredHeight ? factorHeight : factorWidth;
+		
+		int finalWidth = (int) (img.getWidth() * factor);
+		int finalHeight = (int) (img.getHeight() * factor);
+		
+		int imageX = (desiredWidth - finalWidth) / 2;
+		int imageY = (desiredHeight - finalHeight) / 2;
+		Graphics2D g = resized.createGraphics(); // this generates a black background
+		g.drawImage(img, imageX, imageY, finalWidth, finalHeight, null);
+		g.dispose();
 		
 		return resized;
 		

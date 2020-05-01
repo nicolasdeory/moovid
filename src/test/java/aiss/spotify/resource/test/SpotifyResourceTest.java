f73512f3fc76ebcb37a50c36706e101ea7cb8a1d
Nicolás de Ory <deorynicolas@gmail.com>
2020-05-01 16:49:23 +0200

    Edited spotify tests

--- src/test/java/aiss/spotify/resource/test/SpotifyResourceTest.java
+++ src/test/java/aiss/spotify/resource/test/SpotifyResourceTest.java
@@ -1,5 +1,7 @@
 package aiss.spotify.resource.test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -13,10 +15,11 @@ public class SpotifyResourceTest {
 	private static Logger log = Logger.getLogger(SpotifyResourceTest.class.getName());
 	
 	@Test
-	public void testGetName() throws Exception{
+	public void testGetArtistIds() throws Exception{
 		if (!SpotifyResource.isAuthorized())
 			SpotifyResource.authorize();
-		List<Artist> art = SpotifyResource.testGetArtist();
-		log.info("List of artists: " + art);
+		List<Artist> art = SpotifyResource.getArtistIds("zedd,savant");
+		assertEquals(art.get(0).getId(),"2qxJFvFYMEDqd7ui6kSAcq");
+		assertEquals(art.get(1).getId(),"5RBdF1pJSLF3ugc2Y2PoB8");
 	}
 }

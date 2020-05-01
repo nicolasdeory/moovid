722c3084c70ebbc5609eed41277b7eecc336ae1c
Nicolás de Ory <deorynicolas@gmail.com>
2020-05-01 16:48:43 +0200

    Fixed Spotify Authorization

--- src/main/java/aiss/spotify/model/AccessToken.java
+++ src/main/java/aiss/spotify/model/AccessToken.java
@@ -2,15 +2,21 @@ package aiss.spotify.model;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AccessToken {
+	
 	@JsonProperty("access_token")
 	private String accessToken;
+	
 	@JsonProperty("token_type")
 	private String tokenType;
+	
 	@JsonProperty("expires_in")
 	private Integer expiresIn;
+	
 	private LocalDateTime creationTimestamp;
 	public String getAccessToken() {
 		return accessToken;
--- src/main/java/aiss/spotify/resource/SpotifyResource.java
+++ src/main/java/aiss/spotify/resource/SpotifyResource.java
@@ -8,21 +8,23 @@ import java.time.Period;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 import org.restlet.data.ChallengeResponse;
 import org.restlet.data.ChallengeScheme;
 import org.restlet.data.Form;
+import org.restlet.data.Method;
 import org.restlet.representation.Representation;
 import org.restlet.resource.ClientResource;
 import org.restlet.resource.ResourceException;
+import org.restlet.security.ChallengeAuthenticator;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import aiss.spotify.model.AccessToken;
 import aiss.spotify.model.Artist;
-import aiss.spotify.model.Auth;
 import aiss.spotify.model.Song;
 
 public class SpotifyResource {
@@ -33,7 +35,7 @@ public class SpotifyResource {
 		if (token == null)
 			return false;
 		else {
-			long time = token.getCreationTimestamp().until(LocalTime.now(), ChronoUnit.SECONDS);
+			long time = token.getCreationTimestamp().until(LocalDateTime.now(), ChronoUnit.SECONDS);
 			return time < token.getExpiresIn();
 		}
 	}
@@ -42,19 +44,18 @@ public class SpotifyResource {
        ClientResource cr1 = null;
         try {
             cr1 = new ClientResource("https://accounts.spotify.com/api/token");
-            Auth auth = new Auth("client_credentials");
-            ChallengeResponse chres = new ChallengeResponse(new ChallengeScheme("", ""));
-            chres.setRawValue("e3e7195d0d294ee18a9154414f0cd36e:"
-            		+ "f3c5a5f240724e53b682b04aeca8a87a");
             Form form = new Form();
             form.set("grant_type", "client_credentials");
             Representation repr = form.getWebRepresentation();
+            ChallengeResponse chres = new ChallengeResponse(
+            				ChallengeScheme.HTTP_BASIC,"e3e7195d0d294ee18a9154414f0cd36e", "f3c5a5f240724e53b682b04aeca8a87a");
             cr1.getRequest().setChallengeResponse(chres);
-            //cr1.addQueryParameter("grant_type", "client_credentials");
-            //cr1.getRequest().getHeaders().add("Authorization", );
             cr1.setEntityBuffering(true);
-            AccessToken uploadToken = cr1.post(repr, AccessToken.class);
-            uploadToken.setCreationTimestamp(LocalDateTime.now());
+            cr1.setMethod(Method.POST);
+            cr1.getRequest().setEntity(repr);
+            cr1.getRequestEntity();
+            token = cr1.post(repr, AccessToken.class);
+            token.setCreationTimestamp(LocalDateTime.now());
         } catch (ResourceException re){
         	//TODO: logger
             System.out.println("Error authorizing Spotify");
@@ -87,18 +88,18 @@ public class SpotifyResource {
 				.next().get("id").textValue());
 	}
 	
-	public static List<Artist> testGetArtist() {
-		String artists = "zedd,savant";
+	public static List<Artist> getArtistIds(String artists) {
+		if (!isAuthorized()) authorize();
 		String[] artist_split = artists.split(",");
 		List<Artist> id = new ArrayList<Artist>();
 		for (String artist : artist_split) {
 			String uri_artist = "https://api.spotify.com/v1/search?q=";
-			uri_artist += artist + "&type=artist";
+			uri_artist += artist.trim() + "&type=artist";
 			ClientResource cr_ar = new ClientResource(uri_artist);
-			ChallengeResponse chres = new ChallengeResponse(new ChallengeScheme("", ""));
-            chres.setRawValue(token.getAccessToken());
-            cr_ar.getRequest().setChallengeResponse(chres);
-			//cr_ar.getRequest().getHeaders().add("Authorization", token.getAccessToken());
+			ChallengeResponse chres = new ChallengeResponse(
+    				ChallengeScheme.HTTP_OAUTH_BEARER);
+			chres.setRawValue(token.getAccessToken());
+			cr_ar.getRequest().setChallengeResponse(chres);
 			String json_artist = cr_ar.get(String.class);
 			id.add(SpotifyResource.getArtistFromJson(json_artist));
 		}

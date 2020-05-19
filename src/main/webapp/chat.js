9bd3f659d6d4993435b52047fea1b7c1f5b1954b
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2020-05-19 17:45:13 +0200

    created function on chat.js and minor fixes

--- src/main/webapp/chat.js
+++ src/main/webapp/chat.js
@@ -1,25 +1,6 @@
-
-var messages = [], //array that hold the record of each string in chat
-  lastUserMessage = "", //keeps track of the most recent input string from the user
-  botMessage = "", //var keeps track of what the chatbot is going to say
-  botName = 'Chatbot', //name of the chatbot
-
-
-
-//edit this function to change what the chatbot says
-function chatbotResponse() {
-
-  botMessage = "CONFIRMO"; //the default message
-
-
-}
-
-//this runs each time enter is pressed.
-//It controls the overall input and output
-
 function newEntry() {
   //if the message from the user isn't empty then run 
-  if (document.getElementById("chatbox").value != "") {
+  if (document.getElementById("chatbox").value != "" ) {
     //pulls the value from the chatbox ands sets it to lastUserMessage
     lastUserMessage = document.getElementById("chatbox").value;
     //sets the chat box to be clear
@@ -28,20 +9,18 @@ function newEntry() {
   }
 }
 
-
 //runs the keypress() function when a key is pressed
 document.onkeypress = keyPress;
 //if the key pressed is 'enter' runs the function newEntry()
 function keyPress(e) {
-    document.getElementById("chatbox").value = "";
   var x = e || window.event;
   var key = (x.keyCode || x.which);
   if (key == 13 || key == 3) {
     //runs this function when enter is pressed
     newEntry();
   }
-}
-
-function Presionar(){
-    alert("You pressed a key inside the input field");
+  if (key == 38) {
+    console.log('hi')
+      //document.getElementById("chatbox").value = lastUserMessage;
+  }
 }

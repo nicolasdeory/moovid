05ed998db4bf45ada0e25c2dd2589cce39fc848c
Nicolás de Ory <deorynicolas@gmail.com>
2020-05-24 17:02:01 +0200

    Added basic api description comments

--- src/main/java/aiss/api/resources/SongResource.java
+++ src/main/java/aiss/api/resources/SongResource.java
@@ -2,7 +2,7 @@ package aiss.api.resources;
 
 import javax.ws.rs.Path;
 
-@Path("/songs")
+@Path("/song")
 public class SongResource {
 	
 	public static SongResource _instance=null;
@@ -13,6 +13,10 @@ public class SongResource {
 			_instance=new SongResource();
 		return _instance; 
 	}
-
+	
+	// PUT - subir un mp3 /song/uploadmp3
+	// GET - mp3 song id /song/getmp3
+	// POST - search mp3 song /song/searchmp3
+	// DELETE - borrar mp3 con id /song/deletemp3
 
 }
